90377dece66c44e910bd4eb77810060862ca4499
Alican Uver <can.uverr@gmail.com>
2020-07-17 01:24:38 +0300

    general css written

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Login from './Login';
+import Error from './Error';
+import Dashboard from './Dashboard';
+import AuthWrapper from './AuthWrapper';
+
+export { Login, Error, Dashboard, AuthWrapper };
